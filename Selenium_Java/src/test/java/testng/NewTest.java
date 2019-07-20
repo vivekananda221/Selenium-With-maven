25db9433d4f6553a08cb453c69422cb4f1504ac7
vicky <vicky@192.168.1.39>
2019-07-20 11:41:24 +0530

    Selenium with maven

---
+++ Selenium_Java/src/test/java/testng/NewTest.java
@@ -0,0 +1,30 @@
+package testng;
+
+import org.testng.annotations.Test;
+
+public class NewTest 
+{
+  @Test
+  public void f() 
+  {
+	 System.out.println("I am method f");
+  }
+  
+  @Test
+  public void f1() 
+  {
+	  System.out.println("I am method f1");
+  }
+  
+  @Test
+  public void f2() 
+  {
+	  System.out.println("I am method f2");
+  }
+  
+  @Test
+  public void f3() 
+  {
+	  System.out.println("I am method f3");
+  }
+}
